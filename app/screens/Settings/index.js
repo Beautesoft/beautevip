b6ac7336c676e9ae8cab1823f6a624a4075ecee1
nellaibill <nellaibill@gmail.com>
2023-06-13 19:01:57 +0800

    release-1.1.8 and address null fix added

--- app/screens/Settings/index.js
+++ app/screens/Settings/index.js
@@ -100,12 +100,12 @@ export default function Settings({ navigation }) {
         // });
         console.log("addressjson", json);
         try {
-          setaddress(json?.result?.[0] ?? "");
+          setaddress(json?.result?.[0] !== null && json?.result?.[0] !== undefined ? json.result[0] : "");
         } catch (error) { }
         try {
-          userData.customerAddress = json?.result?.[0].address ?? "";
+          userData.customerAddress = json?.result?.[0].address !== null && json?.result?.[0].address !== undefined ? json.result[0].address : "";
           dispatch(updateUserData(userData));
-          listArr[2].value = json?.result?.[0].address ?? "";
+          listArr[2].value = json?.result?.[0].address !== null && json?.result?.[0].address !== undefined ? json.result[0].address : "";
           setState(listArr);
         } catch (error) { }
 
