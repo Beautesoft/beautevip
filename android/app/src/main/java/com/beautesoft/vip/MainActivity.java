f20b3c8d564e0f08c91cadd5a246a95dabcc39bb
Saleem <nellaibill@gmail.com>
2023-09-20 12:24:09 +0530

    modified package appname for kirei

--- android/app/src/main/java/com/beautesoft/vip/MainActivity.java
+++ android/app/src/main/java/com/beautesoft/vip/MainActivity.java
@@ -1,4 +1,4 @@
-package com.beautesoft.nailqueen;
+package com.beautesoft.kirei;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
@@ -17,6 +17,6 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected String getMainComponentName() {
-    return "nailqueen";
+    return "kirei";
   }
 }
