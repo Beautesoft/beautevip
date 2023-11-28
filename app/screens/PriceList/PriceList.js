ca856073295f0c2f9427744559f875e43968164f
Saleem <nellaibill@gmail.com>
2023-11-28 08:39:04 +0530

    Kirei:Pricelist -Hardcoded content added

--- app/screens/PriceList/PriceList.js
+++ app/screens/PriceList/PriceList.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { View, TouchableOpacity, Text, FlatList, StyleSheet, Dimensions, Image } from 'react-native';
 import CHeader from '../../components/CHeader';
 import BannerCarousel from '../Shared/Banner/BannerCarousel';
 import { Modal, Pressable } from 'react-native';//this is for android
 import { theme } from '../../redux/reducer/theme';
+import CText from '../../components/CText';
 const { width } = Dimensions.get('window');
 
 const PriceList = ({ route, navigation }) => {
@@ -12,10 +14,12 @@ const PriceList = ({ route, navigation }) => {
   const [priceListBannerImageURL, setPriceListBannerImageURL] = useState("");
   // Assuming the API response looks like this
   const resetFlow = !!route.params?.resetFlow;
+  const { clientDetails } = useSelector((state) => state.auth);
+
   const apiResponse = [
     {
       "bannerID": 9,
-      "bannerName": "Lash By Director",
+      "bannerName": "Director Price List",
       "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_pricelist4.jpeg",
       "bannerDesc": "Lash By Director",
       "termsAndCondition": "",
@@ -23,7 +27,7 @@ const PriceList = ({ route, navigation }) => {
     },
     {
       "bannerID": 5,
-      "bannerName": "Lash By Manager",
+      "bannerName": "Manager Price List",
       "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_Pricelist2.jpeg",
       "bannerDesc": "Lash By Manager",
       "termsAndCondition": "",
@@ -31,7 +35,7 @@ const PriceList = ({ route, navigation }) => {
     },
     {
       "bannerID": 8,
-      "bannerName": "Lash By Staff",
+      "bannerName": "Staff Price List",
       "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_pricelist3.jpeg",
       "bannerDesc": "Lash By Staff",
       "termsAndCondition": "",
@@ -41,58 +45,58 @@ const PriceList = ({ route, navigation }) => {
   ];
 
   const apiResponseForTermsAndCondition = [
-{
-            "bannerID": 6,
-            "bannerName": "Terms and Condition 1",
-            "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_terms1.jpeg",
-            "bannerDesc": "Terms and Condition 1",
-            "termsAndCondition": "",
-            "bannerIcon": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/BookingConditions.png"
-        },
-        {
-            "bannerID": 7,
-            "bannerName": "Terms and Condition 2",
-            "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_terms2.jpeg",
-            "bannerDesc": "Terms and Condition 2",
-            "termsAndCondition": "",
-            "bannerIcon": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/RetentionConditions.png"
-        },
-        {
-            "bannerID": 10,
-            "bannerName": "Terms and Condition 3",
-            "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_terms3.jpeg",
-            "bannerDesc": "Terms and Condition 3",
-            "termsAndCondition": "",
-            "bannerIcon": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/ReschedulePolicy.png"
-        },
-        {
-            "bannerID": 11,
-            "bannerName": "Terms and Condition 4",
-            "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_terms4.jpeg",
-            "bannerDesc": "Terms and Condition 4",
-            "termsAndCondition": "",
-            "bannerIcon": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/TermsAndConditions.png"
-        },
+    {
+      "bannerID": 6,
+      "bannerName": "Booking",
+      "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_terms1.jpeg",
+      "bannerDesc": "Terms and Condition 1",
+      "termsAndCondition": "",
+      "bannerIcon": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/BookingConditions.png"
+    },
+    {
+      "bannerID": 7,
+      "bannerName": "Retention Conditions",
+      "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_terms2.jpeg",
+      "bannerDesc": "Terms and Condition 2",
+      "termsAndCondition": "",
+      "bannerIcon": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/RetentionConditions.png"
+    },
+    {
+      "bannerID": 10,
+      "bannerName": "Reschedule Policy",
+      "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_terms3.jpeg",
+      "bannerDesc": "Terms and Condition 3",
+      "termsAndCondition": "",
+      "bannerIcon": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/ReschedulePolicy.png"
+    },
+    {
+      "bannerID": 11,
+      "bannerName": "Terms and Conditions",
+      "bannerImg": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/Banner_KBHQ_terms4.jpeg",
+      "bannerDesc": "Terms and Condition 4",
+      "termsAndCondition": "",
+      "bannerIcon": "http://sequoiasg.ddns.net:7049/wellness/KBHQ/TermsAndConditions.png"
+    },
     // ... more button data from API
   ];
   const buttonWidth = 0.8 * width; // 80% of screen width
 
   const renderItem = ({ item }) => (
     <TouchableOpacity style={[styles.button, { backgroundColor: 'orange', width: buttonWidth, height: 60 }]} onPress={() => handleButtonPress(item)}>
-      <Text style={styles.buttonText}>{item.bannerDesc}</Text>
+      <Text style={styles.buttonText}>{item.bannerName}</Text>
     </TouchableOpacity>
   );
 
   const renderItemForTermsAndConditions = ({ item }) => (
     <TouchableOpacity style={[styles.button, { backgroundColor: '#70ad47', width: buttonWidth, height: 60 }]} onPress={() => handleButtonPress(item)}>
-      <Text style={styles.buttonText}>{item.bannerDesc}</Text>
+      <Text style={styles.buttonText}>{item.bannerName}</Text>
     </TouchableOpacity>
   );
 
 
   const handleButtonPress = (buttonData) => {
     setPriceListImageModal(true);
-    console.log('Button pressed:', buttonData.bannerDesc);
+    console.log('Button pressed:', buttonData.bannerName);
     setPriceListBannerImageURL(buttonData?.bannerImg);
   };
 
@@ -100,7 +104,7 @@ const PriceList = ({ route, navigation }) => {
   return (
     <>
       <CHeader
-        title={'Price List'}
+        title={type == 'price' ? 'Price List' : type == 'terms' ? 'Terms & Condition' : 'Location'}
         showLeftIcon
         onLeftIconPress={() =>
           navigation.navigate("BottomTabsNavigator")}
@@ -124,6 +128,35 @@ const PriceList = ({ route, navigation }) => {
             renderItem={renderItemForTermsAndConditions}
             numColumns={1} // Use 1 column to display buttons vertically
           />
+        </View>
+      }
+      {type == 'location' &&
+        <View style={{ padding: 10 }}>
+          <View style={{ flexDirection: "row" }}>
+            <View style={{ flex: 0 }}>
+              <Image
+                source={{ uri: clientDetails?.clientLogo }}
+                style={{
+                  borderRadius: 50,
+                  height: 100,
+                  width: 100,
+                  overflow: 'hidden',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'white',
+                }}
+                resizeMode="center"
+              />
+            </View>
+            <View style={{ flex: 1, paddingLeft: 12 }}>
+              <CText value="Kirei Beauty" size={24} />
+              <CText value="All Nett Price !" size={24} />
+            </View>
+
+          </View>
+          <View style={{paddingLeft:10}}>
+            <CText value="Address : 2 Venture drive #02-26, Vision Exchange" size={18} />
+          </View>
         </View>
       }
 
@@ -202,6 +235,18 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     fontSize: 28,
+  },
+
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
