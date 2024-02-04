8e3951f4dd9d2a323a8a27a93e09c1dbd10a5870
Saleem <nellaibill@gmail.com>
2024-02-04 17:31:39 +0530

    Kirei:URL change

--- app/screens/Settings/index.js
+++ app/screens/Settings/index.js
@@ -314,7 +314,7 @@ export default function Settings({ navigation }) {
   });
   listArr.push({
     title: 'Version',
-    value: '28.JAN.2024',
+    value: '04.FEB.2024',
     onPress: () => {
       //navigation.navigate('Language');
     },
